44df6802108f592d1e5507379da2eb6388e28ec2
Talles L <talleslasmar@gmail.com>
2019-09-08 19:49:07 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(name='floaty',
+      version='1.0.1',
+      url='https://github.com/tallesl/py-floaty',
+      author='Talles Lasmar',
+      author_email='talleslasmar@gmail.com',
+      description='Bandages for boto3 floating-point wounds.',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
+      packages=find_packages())

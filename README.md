ad73714da16d647f9409a7cdab361f8c2258d292
Talles L <talleslasmar@gmail.com>
2019-10-24 16:58:46 -0300

    changing the given dict instead of making another

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![](https://badge.fury.io/py/floaty.svg)](https://pypi.org/project/floaty)
 
-Bandages for boto3 floating-point wounds. [*](https://github.com/boto/boto3/issues/665)
+Bandages for boto3 floating-point wounds. [\*](https://github.com/boto/boto3/issues/665)
 
 ```py
 from floaty import *
@@ -10,6 +10,6 @@ from floaty import *
 no_float(some_obj)  # replaces all floats with ints and Decimals
 no_decimal(some_obj)  # replaces all Decimals with ints and floats
 
-read_item(query_return)  # reads up 'Item' from the dictionary and calls no_decimal on it
-read_items(query_return)  # reads up 'Items' from the dictionary and calls no_decimal on each element of it
+read_item(query_return)  # reads up 'Item' from the dictionary, calls no_decimal on it, and returns it
+read_items(query_return)  # reads up 'Items' from the dictionary, calls no_decimal on each element of it, and returns it
 ```
--- floaty/__init__.py
+++ floaty/__init__.py
@@ -3,36 +3,34 @@ from numbers import Number
 
 
 def no_decimal(obj):
-    return _replace_floating_point(obj, float)
+    _replace_floating_point(obj, float)
 
 
 def no_float(obj):
-    return _replace_floating_point(obj, lambda x: Decimal(str(x)))
+    _replace_floating_point(obj, lambda x: Decimal(str(x)))
 
 
 def read_item(query_return):
     item = query_return['Item'] if 'Item' in query_return else None
-    return no_decimal(item)
+    no_decimal(item)
+
+    return item
 
 
 def read_items(query_return):
     items = query_return['Items'] if 'Items' in query_return else []
-    return [no_decimal(i) for i in items]
+
+    for i in items:
+        no_decimal(i)
+
+    return items
 
 
 def _replace_floating_point(obj, replace):
-    if isinstance(obj, bool):
-        return obj
+    for k, v in obj.items():
 
-    if isinstance(obj, list):
-        return [_replace_floating_point(i, replace) for i in obj]
+        if isinstance(v, dict):
+            _replace_floating_point(v, replace)
 
-    elif isinstance(obj, dict):
-        return {k: _replace_floating_point(v, replace) for k, v in obj.items()}
-
-    elif isinstance(obj, Number) and obj % 1:
-        return replace(obj)
-
-    else:
-        return obj
-
+        elif isinstance(v, Number) and v % 1:
+            obj[k] = replace(v)

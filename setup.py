2450f32e9f44e94c955d450bce0f9878390dad81
Talles L <talleslasmar@gmail.com>
2019-10-24 18:27:27 -0300

    2.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='floaty',
-      version='2.0.0',
+      version='2.0.1',
       url='https://github.com/tallesl/py-floaty',
       author='Talles Lasmar',
       author_email='talleslasmar@gmail.com',

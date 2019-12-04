20b513faeb0a76be58762da2c2251c6a65653837
Talles L <talleslasmar@gmail.com>
2019-12-04 12:01:37 -0300

    boolean fix

--- floaty/__init__.py
+++ floaty/__init__.py
@@ -38,11 +38,14 @@ def no_empty_string(obj):
 
 
 def _replace_obj(obj, replace):
-    if isinstance(obj, dict):
-        return _replace_dict(obj, replace)
+    if isinstance(obj, bool):
+        return obj
 
     elif isinstance(obj, Number):
         return _replace_number(obj, replace)
+
+    elif isinstance(obj, dict):
+        return _replace_dict(obj, replace)
 
     elif isinstance(obj, list):
         return _replace_list(obj, replace)
@@ -51,15 +54,15 @@ def _replace_obj(obj, replace):
         return obj
 
 
+def _replace_number(n, replace):
+    return replace(n) if n % 1 else int(n)
+
+
 def _replace_dict(d, replace):
     for k, v in d.items():
         d[k] = _replace_obj(v, replace)
 
     return d
-
-
-def _replace_number(n, replace):
-    return replace(n) if n % 1 else int(n)
 
 
 def _replace_list(l, replace):
